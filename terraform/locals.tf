f3f9f65c1e81c74d391699760337f4a5c3ab7768
Шумский Виктор <victorsh@electro.local>
2025-06-19 15:08:43 +0300

    final!

--- terraform/locals.tf
+++
@@ -1,9 +0,0 @@
-# сохраняем ключ в локальные перменные
-locals {
-  ssh_key = file("~/.ssh/ycbarmaq.pub")
-
-}
-# зоны для переиспользвания
-locals {
-  zones = ["ru-central1-a", "ru-central1-b", "ru-central1-d"]
-}
